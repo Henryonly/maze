67699d0b4473391b8fb5ee14294507d63ebd9250
Henryonly <1746023370@qq.com>
2025-03-21 10:33:31 +0800

    TCP build

--- bingyan-maze/bingyan-maze.cpp
+++ bingyan-maze/bingyan-maze.cpp
@@ -6,13 +6,21 @@
 
 // pixel of each box is (20,20)
 
+#ifndef _maze_h_
+#define _maze_h_
+
+#define WIN32_LEAN_AND_MEAN
 #include<conio.h>
 #include<time.h>
 #include<easyx.h>
 #include<stdlib.h>
+#include <WinSock2.h>
+#include <WS2tcpip.h>
 #include<Windows.h>
 #include<graphics.h>
 #include<stdio.h>//used to debug
+
+#pragma comment(lib, "ws2_32.lib")
 
 enum key
 {
@@ -48,6 +56,7 @@ IMAGE imageGPS;//every item in mini map
 POINT disGPS;//where are you in mini map to display?
 clock_t start_time;
 clock_t now_time;
+char name[50];
 
 //FUNCTIONS
 int input(int* keyboard);//regulate activities according to keyboard input
@@ -64,6 +73,12 @@ void up();//if go up
 void down();//if go down
 bool winlema();//whether win
 bool Quit();//quit the game?
+wchar_t* MultiByteToWideCharWrapper(const char* str);//change data type
+int my_inet_pton(int af, const char* src, void* dst);//ipv4 translation
+int text(char* buffer);//text
+int server();//as the server
+int record[10000][100];//database
+
 
 //MACRO DEFINITION
 #define VK_LEFT 0x25
@@ -184,8 +199,9 @@ void game()
 {
 	map_size.cx = map_size.cy = 0;
 	//learned in csdn
-	wchar_t* shuru = new wchar_t[4];
+	wchar_t* shuru = new wchar_t[50];
 	//learned in doubao
+
 	while (map_size.cx < 50 || map_size.cx>300)
 	{
 		InputBox(shuru, 4, L"输入宽度吧！\n范围：50~300", L"输入", L"50");
@@ -202,6 +218,20 @@ void game()
 		WideCharToMultiByte(CP_UTF8, 0, shuru, -1, str, len, NULL, NULL);
 		map_size.cy = atoi(str);
 	}
+	x0:memset(shuru, 0, 50);
+	InputBox(shuru, 50, L"给自己起个好听的名字！不能带阿拉伯数字\n", L"输入", L"");
+	int len = WideCharToMultiByte(CP_UTF8, 0, shuru, -1, NULL, 0, NULL, NULL);
+	char* str = (char*)malloc(len);
+	WideCharToMultiByte(CP_UTF8, 0, shuru, -1, str, len, NULL, NULL);
+	for (int i = 0;i < 50;i++)
+	{
+		if (shuru[i] >= '0' || shuru[i] <= '9')
+		{
+			goto x0;
+		}
+	}
+	strcpy_s(name, str);
+
 	//make the width and height odd:if not,wall of the game security border may break!
 	if (map_size.cx % 2 == 0)
 	{
@@ -409,13 +439,40 @@ bool winlema()
 	if (where_are_you.x == map_size.cx && where_are_you.y == map_size.cy - 1)
 	{
 		HWND hwnd = GetHWnd();
-		if (MessageBox(hwnd, L"挑战成功！\n还要再来一次吗？", L"恭喜", MB_YESNO | MB_ICONQUESTION) == IDYES)
+		now_time = clock();
+		char arr1[1000], arr2[500], arr3[500], arr4[500], arr5[500];
+		strcpy_s(arr1, "挑战成功！\n您的用时是：");
+		int integer = now_time / 1000;
+		int point = now_time % 1000;
+		_itoa_s(integer, arr2, 10);
+		strcpy_s(arr3, ".");
+		_itoa_s(point, arr4, 10);
+		strcpy_s(arr5, "秒，还要再来一次吗？");
+		strcat_s(arr1, arr2);
+		strcat_s(arr1, arr3);
+		strcat_s(arr1, arr4);
+		strcat_s(arr1, arr5);
+		wchar_t* widestr = MultiByteToWideCharWrapper(arr1);
+		if (MessageBoxW(hwnd, widestr, L"恭喜", MB_YESNO | MB_ICONQUESTION) == IDYES)
 		{
+			if (MessageBoxW(hwnd, L"您要查看您的成绩吗？黑方框可以看", L"求求了", MB_YESNO | MB_ICONQUESTION) == IDYES)))
+			{
+				char str1[50];
+				char* str2 = "华科是985";
+				str1 = "127.0.0.1";
+				strcat_s(str1, str2);
+				text(str1);
+				server();
+			}
+			strcat_s(arr1, name);
+			text(arr1);
 			game();
 			return 0;
 		}
 		else
 		{
+			strcat_s(arr1, name);
+			text(arr1);
 			return 1;
 		}
 	}
@@ -427,3 +484,161 @@ bool Quit()
 	HWND hwnd = GetHWnd();
 	return (MessageBox(hwnd, L"别走啊！你真的要丢下我了吗？", L"询问", MB_OKCANCEL | MB_ICONQUESTION) == IDOK);
 }
+
+wchar_t* MultiByteToWideCharWrapper(const char* str) 
+{
+	int len = MultiByteToWideChar(CP_ACP, 0, str, -1, NULL, 0);
+	wchar_t* wstr = (wchar_t*)malloc(len * sizeof(wchar_t));
+	if (wstr) 
+	{
+		MultiByteToWideChar(CP_ACP, 0, str, -1, wstr, len);
+	}
+	return wstr;
+}
+
+int text(char* buffer) 
+{
+	WSADATA wsa;
+	SOCKET client_socket;
+	struct sockaddr_in server_addr;
+	// initialize Winsock
+	if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0)
+	{
+		//if fail
+		printf("WSAStartup失败: %d\n", WSAGetLastError());
+		return 1;
+	}
+
+	// create Socket
+	client_socket = socket(AF_INET, SOCK_STREAM, 0);
+	if (client_socket == INVALID_SOCKET)
+	{
+		//if fail
+		printf("创建Socket失败: %d\n", WSAGetLastError());
+		WSACleanup();
+		return 1;
+	}
+
+	// server's address + port
+	server_addr.sin_family = AF_INET;
+	server_addr.sin_port = htons(5210);
+	if (my_inet_pton(AF_INET, "127.0.0.1", &server_addr.sin_addr) != 1)
+	{
+		//if fail
+		printf("IP地址转换失败\n");
+		closesocket(client_socket);
+		WSACleanup();
+		return 1;
+	}
+
+	// connect the server
+	if (connect(client_socket, (struct sockaddr*)&server_addr, sizeof(server_addr)) == SOCKET_ERROR)
+	{
+		//if fail
+		printf("连接服务器失败: %d\n", WSAGetLastError());
+		closesocket(client_socket);
+		WSACleanup();
+		return 1;
+	}
+
+	// send off messages
+	send(client_socket, buffer, strlen(buffer) + 1, 0);
+	recv(client_socket, buffer, sizeof(buffer), 0);
+	printf("%s\n", buffer);
+
+	// close Socket and clean
+	closesocket(client_socket);
+	WSACleanup();
+	return 0;
+}
+
+int my_inet_pton(int af, const char* src, void* dst) 
+{
+	if (af == AF_INET) 
+	{
+		unsigned char* p = (unsigned char*)dst;
+		int a, b, c, d;
+		if (sscanf_s(src, "%d.%d.%d.%d", &a, &b, &c, &d) == 4 &&
+			a >= 0 && a <= 255 &&
+			b >= 0 && b <= 255 &&
+			c >= 0 && c <= 255 &&
+			d >= 0 && d <= 255) 
+		{
+			p[0] = (unsigned char)a;
+			p[1] = (unsigned char)b;
+			p[2] = (unsigned char)c;
+			p[3] = (unsigned char)d;
+			return 1;
+		}
+	}
+	return 0;
+}
+
+int server()
+{
+	WSADATA wsa;
+	SOCKET server_socket, client_socket;
+	struct sockaddr_in server_addr, client_addr;
+	int addr_len = sizeof(client_addr);
+	char buffer[1024] = { 0 };
+
+	// 初始化 Winsock
+	if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0)
+	{
+		//失败了怎么办
+		printf("WSAStartup失败: %d\n", WSAGetLastError());
+		return 1;
+	}
+
+	// 创建 Socket
+	server_socket = socket(AF_INET, SOCK_STREAM, 0);
+	if (server_socket == INVALID_SOCKET)
+	{
+		//失败了怎么办
+		printf("创建Socket失败: %d\n", WSAGetLastError());
+		WSACleanup();
+		return 1;
+	}
+
+	// 配置服务器地址和端口
+	server_addr.sin_family = AF_INET;
+	server_addr.sin_addr.s_addr = INADDR_ANY;
+	server_addr.sin_port = htons(5210);
+
+	// 绑定 Socket
+	if (bind(server_socket, (struct sockaddr*)&server_addr, sizeof(server_addr)) == SOCKET_ERROR)
+	{
+		//失败了怎么办
+		printf("绑定失败: %d\n", WSAGetLastError());
+		closesocket(server_socket);
+		WSACleanup();
+		return 1;
+	}
+
+	// 监听连接，时刻在听
+	listen(server_socket, 3);
+
+	// 接受客户端连接，听完拿走
+	client_socket = accept(server_socket, (struct sockaddr*)&client_addr, &addr_len);
+	if (client_socket == INVALID_SOCKET)
+	{
+		//失败了怎么办
+		printf("接受连接失败: %d\n", WSAGetLastError());
+		closesocket(server_socket);
+		WSACleanup();
+		return 1;
+	}
+
+	// 接收客户端消息并回传
+	recv(client_socket, buffer, sizeof(buffer), 0);
+	printf("%s\n", buffer);
+	send(client_socket, buffer, strlen(buffer) + 1, 0);
+
+	// 关闭 Socket 并清理
+	closesocket(client_socket);
+	closesocket(server_socket);
+	WSACleanup();
+	return 0;
+}
+
+#endif
